da8aaa7a61f81a701f49b20e35f2e3633d80708d
Daornit <daonrit2@gmail.com>
2020-05-13 11:04:52 +0800

    boostrap added

--- front-end/src/components/partials/Header.jsx
+++ front-end/src/components/partials/Header.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart, faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
+import { Container, Navbar, Form, InputGroup, FormControl, Button } from 'react-bootstrap';
+import './header.css';
 
 class Header extends Component {
   constructor(props){
@@ -9,27 +11,33 @@ class Header extends Component {
   }
   render() {
     return (
-      <header>
-        <div className="header__left">
-          <select name="category">
-            <option value="1">цамц</option>
-            <option value="2">өмд</option>
-            <option value="3">эрэгтэй</option>
-            <option value="4">эмэгтэй</option>
-          </select>
-          <FontAwesomeIcon icon={faSearch}/>
-          <input type="text"/>
-        </div>
+      <Container>
+        <Navbar className="bg-light justify-content-between">
+          <Form inline>
+            <InputGroup>
+              <InputGroup.Prepend>
+                <InputGroup.Text id="basic-addon1">
+                  <FontAwesomeIcon icon={faSearch}/>
+                </InputGroup.Text>
+              </InputGroup.Prepend>
+              <FormControl
+                placeholder="Username"
+                aria-label="Username"
+                aria-describedby="basic-addon1"
+              />
+            </InputGroup>
+          </Form>
 
-        <div className="header__middle">
-          <img src="http://www.gobi.mn/imgs/icons/gobi.png" alt="logo"/>
-        </div>
+          <div className="header__middle">
+            <img src="http://www.gobi.mn/imgs/icons/gobi.png" alt="logo"/>
+          </div>
 
-        <div className="header__right">
-          <FontAwesomeIcon icon={faHeart}/>
-          <FontAwesomeIcon icon={faShoppingBag}/>
-        </div>
-      </header>
+          <div className="header__right">
+            <FontAwesomeIcon icon={faHeart}/>
+            <FontAwesomeIcon icon={faShoppingBag}/>
+          </div>
+        </Navbar>
+      </Container>
     );
   }
 }
