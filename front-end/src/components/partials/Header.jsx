6e0348ac1f534bfde1e4b788b44f725aac8d0fc3
Daornit <daornit2@gmail.com>
2020-05-13 10:30:37 +0800

    header

--- front-end/src/components/partials/Header.jsx
+++ front-end/src/components/partials/Header.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart, faSearch, faShoppingBag } from '@fortawesome/free-solid-svg-icons'
 
 class Header extends Component {
   constructor(props){
@@ -8,7 +10,25 @@ class Header extends Component {
   render() {
     return (
       <header>
-        Header
+        <div className="header__left">
+          <select name="category">
+            <option value="1">цамц</option>
+            <option value="2">өмд</option>
+            <option value="3">эрэгтэй</option>
+            <option value="4">эмэгтэй</option>
+          </select>
+          <FontAwesomeIcon icon={faSearch}/>
+          <input type="text"/>
+        </div>
+
+        <div className="header__middle">
+          <img src="http://www.gobi.mn/imgs/icons/gobi.png" alt="logo"/>
+        </div>
+
+        <div className="header__right">
+          <FontAwesomeIcon icon={faHeart}/>
+          <FontAwesomeIcon icon={faShoppingBag}/>
+        </div>
       </header>
     );
   }
